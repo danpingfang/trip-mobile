f9f07a3319ad3381d874fedb5494c6c2eedf48dc
wushaojie <wsjj>
2016-11-16 21:48:03 +0800

    更新。

---
+++ src/map/components/LinePlaceBar.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="line-place-bar">
+    <span>总线路</span>
+    <ul class="line-place-list"></ul>
+  </div>
+</template>
---
+++ src/map/components/LinePlaceList.vue
@@ -0,0 +1,11 @@
+<template>
+  <ul class="line-place-list">
+    <li class="line-place-item" v-for="place in list">{{place.targetName}}</li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    props: ['list']
+  };
+</script>
