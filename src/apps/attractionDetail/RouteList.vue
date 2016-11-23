f4ff46a2a9066410cac9156d9dc0474026965a79
fangdanping <fangdanping@oversealive.com>
2016-11-23 14:45:46 +0800

    更新attractionDetail

--- src/apps/attractionDetail/RouteList.vue
+++ src/apps/attractionDetail/RouteList.vue
@@ -1,36 +1,93 @@
 <template>
-  <li class="routes-container" v-for="item in list">
-    <a href="/tls/open/web/line/share?lineId={{line.lineId}}">
-      <div class="routes-cover">
-        <p class="routes-title">{{item.title}}</p>
-        <p class="routes-attractions" v-for="passDestination in item.passDestinations">
-          {{passDestination}}<em v-if="{{velocityCount}} != {{item.passDestinations.size()">／</em>>
-        </p>
-      </div>
-      <div class="extra-info">
-        <div class="important-information">
-          <span class="evaluate-days">预计天数：{{item.dayCount}}日游</span>
-          <span class="fit-month">适合月份：
-            <em v-for="bestTime in item.bestTimes">{{bestTime}}</em>
-          </span>
-          <span class="evaluate-fee">预计费用：￥{{item.cost}}元</span>
-        </div>
-        <div class="tags" v-if="item.tags != ''">
-          <span class="tag-item" v-for="tag in item.tags">{{tag}}</span>
-        </div>
-        <div class="author">
-          <img :src="item.createUser.avatar"
-               alt="{{item.createUser.nickname}}" width="14" height="14"/>
-          <span class="name-time">{{item.createUser.nickname}} / {{item.createTime}}</span>
-        </div>
-      </div>
-    </a>
-    <div class="line-space"></div>
-  </li>
+  <div class="routes">
+    <p>{{data.lineCount}}篇线路提到{{poiname}}</p>
+    <ul class="routes-list" v-if="data.lineCount && data.lineCount !== 0">
+      <li class="routes-container" v-for="list in data.lines.list">
+        <a href="">
+          <div class="routes-cover">
+            <p class="routes-title">{{list.title}}</p>
+            <p class="routes-attractions" v-for="$passDestination in list.passDestinations">
+              {{passDestination}}<em v-if="velocityCount !== list.passDestinations.size()">/</em>
+            </p>
+          </div>
+          <div class="extra-info">
+            <div class="important-information">
+              <span class="evaluate-days">预计天数：{{list.dayCount}}日游</span>
+              <span class="fit-month">适合月份：
+                <em v-if="bestTime in list.bestTimes">{{bestTime}}</em>
+              </span>
+              <span class="evaluate-fee">预计费用：￥{{list.cost}}元</span>
+            </div>
+            <div class="tags" v-if="list.tags !== ''">
+              <span class="tag-item" v-for="tag in list.tags">{{tag}}</span>
+            </div>
+            <div class="author">
+              <img :src="list.createUser.avatar"
+                   alt="{{list.createUser.nickname}}" width="14" height="14"/>
+              <span class="name-time">{{list.createUser.nickname}}/{{list.createTime}}</span>
+            </div>
+          </div>
+        </a>
+        <div class="line-space"></div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+  import $ from 'jquery';
+  import config from '../../config';
+  import Spinner from '../../components/Spinner';
+  import Empty from '../../components/Empty';
+  import LoadEnd from '../../components/LoadEnd';
+
+  const jsConfig = window.jsConfig;
+  const poiName = jsConfig.poiName;
+  const poiId = jsConfig.poiId;
+  const poiGroup = jsConfig.poiGroup;
+  const source = jsConfig.source;
+  const poiType = jsConfig.poiType;
+
   export default {
-    props: ['list']
+    data() {
+      return {
+        poiName,
+        data: null,
+        currentItem: null,
+        isEmpty: false,
+        isEnd: false,
+        emptyText: '没有数据哦～',
+        endText: '没有更多了',
+        loading: true,
+        startIndex: 0,
+        itemCount: 10,
+        busy: false
+      };
+    },
+    components: {
+      Empty,
+      LoadEnd,
+      Spinner
+    },
+    methods: {
+      getLineList() {
+        $.ajax({
+          url: `${config.authApiUrl}/tls/open/api/v1/line/poi_line`,
+          dataType: 'json',
+          data: Object.assign({}, config.mock, {
+            startIndex: this.startIndex,
+            itemCount: this.itemCount,
+            poiId,
+            poiGroup,
+            source,
+            poiType
+          })
+        }).done((response) => {
+          if (response.message === 'success') {
+            this.data = response.data;
+          }
+        });
+      }
+    }
   };
 </script>
